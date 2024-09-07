7b6857358af1f6031fca2507d8053fd70de9fc80
Aaraiz <aaraizzad40@gmail.com>
2024-09-07 17:06:11 +0500

    updatedLab3Q3.c

--- PF lab 3/Lab3Q3.c
+++ PF lab 3/Lab3Q3.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 int main (){
-    int Income , Tax , Salary;
+    int Income , Tax , Salary , TaxRate , C;
     
     // TAKING Input
 printf("Total Salary : ");
 scanf("%d", &Income);
+printf("Tax Rate : ");
+scanf("%d", &TaxRate);
 
 // Calculating Tax 10% And Remaining Salary
-Tax=Income*0.1;
+C=Income*TaxRate;
+Tax=C/100;
 Salary=Income+ -Tax;
 
 // Displaying Tax and Remaining Salary
