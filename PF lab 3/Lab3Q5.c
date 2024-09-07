a008431ad2214eab6dc142a2821b8de1a64a1cb6
Aaraiz <aaraizzad40@gmail.com>
2024-09-07 20:27:15 +0500

    added a new file Lab3Q5.c

--- PF lab 3/Lab3Q5.c
+++ PF lab 3/Lab3Q5.c
@@ -2,28 +2,25 @@
 int main(){
     float P, R, T, SI;
 
-    do
-    {
-        printf("Principal Amount : ");
+    do{
+        printf("Principal Amount : \n");
         scanf("%f", &P);
-        printf("Rate Of Interest : ");
+        printf("Rate Of Interest : \n");
         scanf("%f", &R);
-        printf("Time Period : ");
+        printf("Time Period : \n");
         scanf("%f", &T);
 
-        if (P<100 || P>1000000 && R<5 || R>10 && T<1 || T>10 );
+        if ((P<100 || P>1000000) || (R<5 || R>10) || (T<1 || T>10 ))
 
         {
-            printf("Invalid Input. Please choose numbers from the range.");
+            printf("Invalid Input. Please choose numbers from the range.\n");
         }
-        
-
-    } while (P<100 || P>1000000 && T<5 || T>10 && T<1 || T>10);
+        } while ((P<100 || P>1000000) || (R<5 || R>10) || (T<1 || T>10 ));
 
     //Calculations
     SI=(P*R*T)/100;
 
     //Displaying
-    printf("Simple Interest=%f", SI);
+    printf("Simple Interest = %f\n", SI);
     return 0;
 }
